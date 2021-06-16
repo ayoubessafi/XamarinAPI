22c420ce7f99ca2066a431fe49faf86255c65bda
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2021-06-16 15:04:11 +0100

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,88 @@
+﻿using BookAPI.Models;
+using BookAPI.Repositories;
+using ImageUploader;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _repository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _repository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _repository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            return await _repository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>>PostBooks([FromBody] Book book)
+        {
+            var newBook = await _repository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.Id }, newBook);
+            
+           
+            
+                var stream = new MemoryStream(book.ImageArray);
+                var guid = Guid.NewGuid().ToString();
+                var file = $"{guid}.jpg";
+                var folder = "wwwroot";
+                var response = FilesHelper.UploadImage(stream, folder, file);
+                if (!response)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    book.ImageUrl = file;
+                    await _repository.Create(book);
+                    
+                    return StatusCode(StatusCodes.Status201Created);
+                }
+          
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Book book)
+        {
+            if(id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            await _repository.Update(book);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete (int id)
+        {
+            var bookToDelete = await _repository.Get(id);
+            if (bookToDelete == null)
+                return NotFound();
+
+            await _repository.Delete(bookToDelete.Id);
+            return NoContent();
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        [NotMapped]
+        public byte[] ImageArray { get; set; }
+    }
+}
